15a4fc802c4f7a2dda76d2b84b81c52c0f448b8b
Atul <atul007414@gmail.com>
2026-06-20 05:52:14 +0530

    Added Modules to startApp

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,11 +2,21 @@ import { startApp } from 'modelence/server';
 import playerModule from './modules/playersModule';
 import configModule from './modules/configModule';
 import weeklyRewardModule from './modules/weeklyRewardModule';
+import battleMatchModule from './modules/battleMatchModule';
+import friendshipModule from './modules/friendshipModule';
+import gameSessionModule from './modules/gameSessionModule';
+import suiModule from './modules/suiModule';
+import chiTransactionModule from './modules/chiTransactionModule';
 
 startApp({
     modules: [
         playerModule,
         configModule,
-        weeklyRewardModule
+        weeklyRewardModule,
+        battleMatchModule,
+        friendshipModule,
+        chiTransactionModule,
+        gameSessionModule,
+        suiModule,
     ]
 });
